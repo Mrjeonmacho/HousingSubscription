2ea783850e88b6c4c1d6fbb43b43c355ccc713b4
DESKTOP-6OO0P6F\SSAFY <qqjiwoon@naver.com>
2026-01-23 15:51:29 +0900

    Feat: Playground 페이지 생성

--- Front/src/app/App.tsx
+++ Front/src/app/App.tsx
@@ -2,6 +2,7 @@ import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Layout from "../components/layout/Layout";
 import HomePage from "../pages/HomePage";
 import NoticesPage from "../pages/NoticesPage";
+import Playground from "../pages/Playground";
 
 export default function App() {
   return (
@@ -10,6 +11,7 @@ export default function App() {
         <Routes>
           <Route path="/" element={<HomePage />} />
           <Route path="/notices" element={<NoticesPage />} />
+          <Route path="/playground" element={<Playground />} />
         </Routes>
       </Layout>
     </BrowserRouter>
